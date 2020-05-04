a3282891a7a7b456dc615078093b0c8829cf64d7
LordMorgoth <lord.morgoth@yahoo.com>
2020-05-04 09:26:24 +0200

    Initial commit

---
+++ opencvTry/opencvTry/try.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	Mat img = imread("Lena.jpg");
+	namedWindow("image", WINDOW_NORMAL);
+	imshow("image", img);
+	waitKey(0);
+	return 0;
+}
